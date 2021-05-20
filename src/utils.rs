d6edddcbac5cfbb8a42bee745bb9c22db702f5dc
Christian Swinehart <drafting@samizdat.co>
2021-05-20 17:23:01 -0400

    added decoder for cursor icon names

--- src/utils.rs
+++ src/utils.rs
@@ -876,3 +876,46 @@ pub fn from_key_code(code:VirtualKeyCode) -> String{
     VirtualKeyCode::Cut => "Cut",
   }.to_string()
 }
+
+use glutin::window::CursorIcon;
+pub fn to_cursor_icon(cursor_name:&str) -> Option<CursorIcon> {
+  let cursor = match cursor_name.to_lowercase().as_str() {
+    "default" => CursorIcon::Default,
+    "crosshair" => CursorIcon::Crosshair,
+    "hand" => CursorIcon::Hand,
+    "arrow" => CursorIcon::Arrow,
+    "move" => CursorIcon::Move,
+    "text" => CursorIcon::Text,
+    "wait" => CursorIcon::Wait,
+    "help" => CursorIcon::Help,
+    "progress" => CursorIcon::Progress,
+    "not-allowed" => CursorIcon::NotAllowed,
+    "context-menu" => CursorIcon::ContextMenu,
+    "cell" => CursorIcon::Cell,
+    "vertical-text" => CursorIcon::VerticalText,
+    "alias" => CursorIcon::Alias,
+    "copy" => CursorIcon::Copy,
+    "no-drop" => CursorIcon::NoDrop,
+    "grab" => CursorIcon::Grab,
+    "grabbing" => CursorIcon::Grabbing,
+    "all-scroll" => CursorIcon::AllScroll,
+    "zoom-in" => CursorIcon::ZoomIn,
+    "zoom-out" => CursorIcon::ZoomOut,
+    "e-resize" => CursorIcon::EResize,
+    "n-resize" => CursorIcon::NResize,
+    "ne-resize" => CursorIcon::NeResize,
+    "nw-resize" => CursorIcon::NwResize,
+    "s-resize" => CursorIcon::SResize,
+    "se-resize" => CursorIcon::SeResize,
+    "sw-resize" => CursorIcon::SwResize,
+    "w-resize" => CursorIcon::WResize,
+    "ew-resize" => CursorIcon::EwResize,
+    "ns-resize" => CursorIcon::NsResize,
+    "nesw-resize" => CursorIcon::NeswResize,
+    "nwse-resize" => CursorIcon::NwseResize,
+    "col-resize" => CursorIcon::ColResize,
+    "row-resize" => CursorIcon::RowResize,
+    _ => return None
+  };
+  Some(cursor)
+}
